e2aa44ebe9a8d00c5dd85d99d5280b46c1b02049
Kaka Hoang Huy <kaka.hoanghuy@gmail.com>
2020-04-24 23:12:33 +0900

    Init commit

---
+++ SweetyMemories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SweetyMemories
+//
+//  Created by Tran Dinh Hoang Huy on 2020/04/24.
+//  Copyright © 2020 KakaHoangHuy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
